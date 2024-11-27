849fc39fcbd3545b69b61ad0d632fecf457b36b9
Králik Norbert <kraaliknorbert@gmail.com>
2024-11-28 00:36:15 +0100

    skeleton note list and chat window

--- components/chat/chat-container.js
+++ components/chat/chat-container.js
@@ -4,7 +4,7 @@ import ChatWindow from "./chat-window";
 
 export default function ChatContainer({ topicId }) {
   return (
-    <div className='grid grid-cols-12 min-h-[calc(100vh-16rem)] gap-4'>
+    <div className='grid grid-cols-12 h-[calc(100vh-16rem)] gap-4'>
       <div className='col-span-6'>
         <ChatWindow topicId={topicId} />
       </div>
