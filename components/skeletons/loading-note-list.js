eaa647c5a32b318d116940f6e4e90e2114e9d765
Králik Norbert <kraaliknorbert@gmail.com>
2024-11-28 00:32:47 +0100

    skeleton note list

---
+++ components/skeletons/loading-note-list.js
@@ -0,0 +1,20 @@
+import { Skeleton } from "../ui/skeleton";
+
+export default function LoadingDocList() {
+  return (
+    <div className='col-span-6 flex flex-col items-start gap-2'>
+      <div className="w-1/4 h-12">
+        <Skeleton className="h-full rounded-md bg-slate-100" />
+      </div>
+      <div className="w-full h-12">
+        <Skeleton className="h-full rounded-md bg-slate-100" />
+      </div>
+      <div className="w-full h-12">
+        <Skeleton className="h-full rounded-md bg-slate-100" />
+      </div>
+      <div className="w-full h-12">
+        <Skeleton className="h-full rounded-md bg-slate-100" />
+      </div>
+    </div>
+  )
+}
