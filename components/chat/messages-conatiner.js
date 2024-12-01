6750459196a6a7407fc999df125c383ea24140bd
Králik Norbert <kraaliknorbert@gmail.com>
2024-12-02 00:21:08 +0100

    chat font size

--- components/chat/messages-conatiner.js
+++ components/chat/messages-conatiner.js
@@ -8,11 +8,11 @@ export default function MessagesConatiner({ messagesEndRef, messages, isPending 
   return (
     <div className='max-h-[calc(100vh-23.75rem)] overflow-y-auto flex flex-col space-y-2 pr-2'>
       {messages && messages.map(({ role, content }, index) => {
-        const icon = role === 'user' ? <User /> : <Bot />
+        const icon = role === 'user' ? <User size={16} /> : <Bot size={16} />
         return <div key={index} className={`flex ${role === "user" ? "justify-end" : "justify-start"}`}>
           <div className={`flex gap-2 px-4 py-3 items-start w-4/5 rounded-lg ${role === "user" ? 'border' : "bg-zinc-100"}`}>
             <span>{icon}</span>
-            <p>{content}</p>
+            <p className="text-xs">{content}</p>
           </div>
         </div>
 
